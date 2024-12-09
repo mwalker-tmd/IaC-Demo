5a4c7338c25fe866e6d678be125c05b0a7ad5198
mwalker-tmd <mlwtlw22@gmail.com>
2024-12-09 12:36:54 -0700

    Initial creation of the security-groups module.

---
+++ security-groups/variables.tf
@@ -0,0 +1,10 @@
+variable "vpc_id" {
+  type = string
+  description = "VPC Identifier"
+  default = "ERROR: Security group variable vpc_id not defined."
+}
+variable "trusted_cidr_blocks" {
+  type = list(string)
+  description = "List of CIDR blocks to allow traffic from"
+  default = ["136.36.138.110"]
+}
