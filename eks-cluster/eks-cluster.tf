eece9714d39dfabde04a76938d0dc1f52b09c4b3
mwalker-tmd <mlwtlw22@gmail.com>
2024-12-09 17:03:22 -0700

    Creation of the worker_nodes_role IAM role resource.

--- eks-cluster/eks-cluster.tf
+++ eks-cluster/eks-cluster.tf
@@ -17,6 +17,8 @@ variable "ec2_assume_role_policy" {
 POLICY
 }
 
+
+### Control Plane Nodes IAM Role configuration ###
 resource "aws_iam_role" "control_plane_nodes_role" {
   name = "IaC_Demo-control_plane_nodes_role"
   assume_role_policy = var.ec2_assume_role_policy
@@ -33,6 +35,40 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.control_plane_nodes_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
+
+
+### Worker Nodes IAM Role configuration ###
+resource "aws_iam_role" "worker_nodes_role" {
+  name = "IaC_Demo-worker_nodes_role"
+  assume_role_policy = var.ec2_assume_role_policy
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.worker_nodes_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.worker_nodes_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.worker_nodes_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "AutoScalingFullAccess" {
+  role       = aws_iam_role.worker_nodes_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
+}
+
+resource "aws_iam_instance_profile" "worker_nodes_profile" {
+  role       = aws_iam_role.worker_nodes_role.name
+  name       = "IaC_Demo-worker_nodes_profile"
+  depends_on = [aws_iam_role.worker_nodes_role]
+}
+
